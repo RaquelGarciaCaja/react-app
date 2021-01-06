2aabbeeb4fc63363c19aacbe04e064b9a5b39bcd
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-06 11:53:15 +0100

    add scss Students component

--- src/components/Students.js
+++ src/components/Students.js
@@ -9,32 +9,40 @@ const Students = () => {
       </h2>
       <ul className="students--list">
         <li className="students--check">
-          <i className="students--icon fas fa-check-circle"></i>
-          Standards-based writing practice
+          <div className="students--containerTitle">
+            <i className="students--icon fas fa-check-circle"></i>
+            <h4 className="students--title"> Standards-based writing practice</h4>
+          </div>
           <p className="students--p">
             While creating their story, students will strengthen their use of effective techniques,
             descriptive details, and clear event sequences.
           </p>
         </li>
         <li className="students--check ">
-          <i className="students--icon fas fa-check-circle"></i>
-          Personalized Writing Feedback
+          <div className="students--containerTitle">
+            <i className="students--icon fas fa-check-circle"></i>
+            <h4 className="students--title"> Personalized Writing Feedback</h4>
+          </div>
           <p className="students--p">
             Providing students individualized feedback on their own work is the most appropriate and
             effective approach to teach writing.
           </p>
         </li>
         <li className="students--check ">
-          <i className="students--icon fas fa-check-circle"></i>
-          Online Testing Preparation
+          <div className="students--containerTitle">
+            <i className="students--icon fas fa-check-circle"></i>
+            <h4 className="students--title">Online Testing Preparation</h4>
+          </div>
           <p className="students--p">
             It’s never too soon to prepare students for the online testing environment where they’ll
             be expected to produce their best writing.
           </p>
         </li>
         <li className="students--check ">
-          <i className="students--icon fas fa-check-circle"></i>
-          Transferable Writing Skills
+          <div className="students--containerTitle">
+            <i className="students--icon fas fa-check-circle"></i>
+            <h4 className="students--title"> Transferable Writing Skills</h4>
+          </div>
           <p className="students--p">
             Storywriting offers a plethora of transferable writing skills that will boost their
             ability in both fiction and non-fiction writing assignments.
