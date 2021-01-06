249792243c7de6df628589d5a4cd0bf7815a6077
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-06 19:41:58 +0100

    add scss timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,9 +8,10 @@ const Timeline = () => {
   return (
     <section className="timeline">
       <h2 className="timeline--title timeline--span">The writing bee timeline</h2>
-      <ul className="timeline--seccion">
+      <ul className="timeline--section">
         <li className="timeline--list">
-          <div className="timeline--line">
+          <div className="timeline--containerLine">
+            <div className="timeline--line"></div>
             <i className="timeline--icon fas fa-circle"></i>
           </div>
           <div className="timeline--block">
@@ -31,7 +32,25 @@ const Timeline = () => {
               </p>
             </div>
           </div>
-          <img className="mr-3 img-fluid" src={win} alt="Finals"></img>
+        </li>
+        <li className="timeline--list">
+          <div className="timeline--containerLine">
+            <div className="timeline--line"></div>
+            <i className="timeline--icon fas fa-circle"></i>
+          </div>
+          <div className="timeline--block">
+            <div className="timeline--containerImg">
+              <img className="mr-3 img-fluid" src={win} alt="win" title="win"></img>
+            </div>
+
+            <div className="timeline--text">
+              <h5 className="timeline--subTitle">December - March 2020 - Qualifying Sessions</h5>
+              <p className="timeline--p">
+                Teachers are welcome to conduct classroom-based qualifying sessions with their
+                students in <span class="timeline--span">Grades</span> 4-6.
+              </p>
+            </div>
+          </div>
         </li>
       </ul>
     </section>
