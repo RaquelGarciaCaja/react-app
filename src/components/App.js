25243a1f2d536aab7e0f0d748a830c0a061e65b7
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-06 11:03:31 +0100

    add component WritingBee html and scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import WritingBee from "./WritingBee";
 
 import "../stylesheets/App.scss";
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <section className="app">
       <Header />
+      <WritingBee />
     </section>
   );
 };
---
+++ src/components/WritingBee.js
@@ -0,0 +1,24 @@
+import React from "react";
+import boomWrite from "../image/boomwriter-white.png";
+
+import "../stylesheets/WritingBee.scss";
+
+const WritingBee = () => {
+  return (
+    <div className="writing">
+      <div className="writing--yearContainer">
+        {/* <img className="writing--scroll" src={scroll} alt="" /> */}
+        <i className="writing--scroll fas fa-scroll"></i>
+        <p className="writing--year">The 2020</p>
+      </div>
+      <h1 className="writing--writing">Writing Bee</h1>
+      <h3 className="writing--kids">Because all kids love writing stories!</h3>
+      <h4 className="writing--powered">Powered by:</h4>
+      <div className="writing--imgContainer">
+        <img className="writing--img" src={boomWrite} alt="boom-write" title="boom-write" />
+      </div>
+      <div className="writing--page"></div>
+    </div>
+  );
+};
+export default WritingBee;
