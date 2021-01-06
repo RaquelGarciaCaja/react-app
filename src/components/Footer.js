1c3ff68d992a0d9c692e93eac9d33b711eb5f606
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-06 17:37:09 +0100

    add footer component html and scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,14 @@ import bee from "../image/bee-white.png";
 import boom from "../image/boomwriter.png";
 import partner from "../image/google-partner.png";
 
-import "../stylesheets/Header.scss";
+import "../stylesheets/Footer.scss";
 
 class Footer extends React.Component {
   render() {
     return (
       <footer className="footer">
         <div className="footer--containerImg">
-          <img
-            className="footer--boom footer--img"
-            src={boom}
-            alt="boom-writer"
-            title="boom-writer"
-          />
+          <img className="footer--boom " src={boom} alt="boom-writer" title="boom-writer" />
           <img
             className="footer--bee footer--img"
             src={bee}
@@ -30,10 +25,14 @@ class Footer extends React.Component {
           />
         </div>
         <div className="footer--footer">
-          <p className=" footer--visit ">The Writing Bee is powered by BoomWriter</p>
+          <p className=" footer--visit ">
+            {" "}
+            <span className="footer--span">The writing bee</span> is powered by{" "}
+            <span className="footer--span">BoomWriter</span>
+          </p>
           <p className="footer--visit">
             To find out more visit
-            <a href="https://boomwriter.com/" className="footer--link">
+            <a href="https://boomwriter.com/" className="footer--link" target="_blank">
               {" "}
               www.boomwriter.com
             </a>
